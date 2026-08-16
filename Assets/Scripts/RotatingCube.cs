b6d8094a3b1afaca4aeddff5ee816c4b42d306bb
Derek Thurn <derek@fake.email>
2026-08-16 06:43:14 -0700

    fix: remove empty Unity message

--- Assets/Scripts/RotatingCube.cs
+++ Assets/Scripts/RotatingCube.cs
@@ -7,9 +7,6 @@ namespace Masonry
         [SerializeField]
         private Vector3 rotationDegreesPerSecond = new(0f, 45f, 0f);
 
-        // Deliberate UNT0001 violation to prove the strict lint gate fails.
-        private void Start() { }
-
         private void Update()
         {
             transform.Rotate(rotationDegreesPerSecond * Time.deltaTime, Space.Self);
